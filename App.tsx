91869e2a2751a01152f148fd1f360aae0666b186
Felipe Viana de Andrade <felipes.andrade@gmail.com>
2022-04-12 07:39:45 -0300

    Implementando fontes e cores

--- App.tsx
+++ App.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
 
 import { Home } from './src/screens/Home';
 
 export default function App() {
+  // Carregando as fontes no hook useFonts.
+  const [ fontsLoaded ] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  });
+  
+  // Caso ainda n tenha carregado as fontes, exibe o loading.
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
   return (
     <Home />
   );
